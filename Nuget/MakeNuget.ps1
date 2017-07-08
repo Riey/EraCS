70475d3dcc025dd2d4fde677c310d157a4b65824
Riey <tkfkzo@daum.net>
2017-07-08 23:09:51 +0900

    Publish nuget 1.2.4

--- Nuget/MakeNuget.ps1
+++ Nuget/MakeNuget.ps1
@@ -1,6 +1,6 @@
 $CoreVersion = "1.2.1"
 $FrontEndWpfVersion = "1.2.1.1"
-$EraConsoleVersion = "1.2.3"
+$EraConsoleVersion = "1.2.4"
 $XamarinVersion = "2.3.4.224"
 
 $PropertiesArg = "xamarin_version=$XamarinVersion;core_version=$CoreVersion;frontend_wpf_version=$FrontEndWpfVersion;console_version=$EraConsoleVersion" 
