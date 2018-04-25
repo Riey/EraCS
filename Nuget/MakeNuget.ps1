714d978de7ac21ceba51545fd0e537cb120789f7
Riey <tkfkzo@daum.net>
2018-04-25 14:22:31 +0900

    Update nuget

--- Nuget/MakeNuget.ps1
+++ Nuget/MakeNuget.ps1
@@ -1,9 +1,9 @@
-$CoreVersion = "1.3.0.0"
-$FrontEndWpfVersion = "1.3.0.0"
-$EraConsoleVersion = "1.4.0.0"
+$CoreVersion = "1.3.3.0"
+$EraConsoleVersion = "1.4.1.0"
+$FrontEndWpfVersion = "1.3.1.0"
 
 $PropertiesArg = "core_version=$CoreVersion;frontend_wpf_version=$FrontEndWpfVersion;console_version=$EraConsoleVersion" 
 
 nuget pack EraCS.UI.FrontEnd.Wpf.nuspec -Version $FrontEndWpfVersion -properties $PropertiesArg
-copy ..\EraCS.Core\bin\Release\Riey.EraCS.Core.1.3.0.nupkg .
-copy ..\EraCS.UI.EraConsole\bin\Release\Riey.EraCS.UI.EraConsole.1.4.0.nupkg .
+copy ..\EraCS.Core\bin\Release\Riey.EraCS.Core.1.3.3.nupkg .
+copy ..\EraCS.UI.EraConsole\bin\Release\Riey.EraCS.UI.EraConsole.1.4.1.nupkg .
